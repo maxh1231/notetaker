7878f43bf1046c91e5a46d67240dac51040db245
maxh1231 <maxhumpherys@gmail.com>
2021-11-26 20:15:32 -0700

    landing page, notes, get notes routes

--- server.js
+++ server.js
@@ -2,9 +2,33 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path')
+
+const db = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+    // res.send(db);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.send(db);
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+
